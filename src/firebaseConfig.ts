cca4aa1b0d229045d41697448bd9ea88085b12db
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-08-19 22:17:33 +0900

    Create firebaseConfig.ts

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,13 @@
+//----- Firebaseの設定ファイル。絶対に変えないこと！ -----//
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBd3YC8b6r05fpo1u9WIXbRnQsLbI5wGIo",
+  authDomain: "kss-festival-admin.firebaseapp.com",
+  projectId: "kss-festival-admin",
+  storageBucket: "kss-festival-admin.appspot.com",
+  messagingSenderId: "739103425598",
+  appId: "1:739103425598:web:f16991d454cfe66edbfd6f"
+};
+
+export { firebaseConfig };
+
